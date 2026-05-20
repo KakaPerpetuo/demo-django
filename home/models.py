45918cefc021e9444e0a40a4cc6396e1d53c69f2
kaka <kakaperpetuo2001@gmail.com>
2026-05-20 14:48:17 -0300

    Concluido ate parte 3

--- home/models.py
+++ home/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Mensagem(models.Model):
     titulo = models.CharField(max_length=120)
     conteudo = models.TextField()
+    autor = models.CharField(max_length=80, default="Anônimo")   # ← novo campo
     criada_em = models.DateTimeField(auto_now_add=True)
+    tag = models.CharField(max_length=80, default="Geral") # ← novo campo
 
     class Meta:
         ordering = ["-criada_em"]
