b685ae5877281cf3f4cbebb55edc916e0dcc90fe
kaka <kakaperpetuo2001@gmail.com>
2026-07-15 15:02:01 -0300

    Finalizacao do roteiro 5

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("sobre/", views.sobre, name="sobre"),
     path("tema/", views.tema, name="tema"),
+    path("nova/", views.nova_mensagem, name="nova_mensagem"),
 ]
